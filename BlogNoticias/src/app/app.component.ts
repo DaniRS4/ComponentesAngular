3bb4568ae2d31306c1eeddbac74461b22af4636f
Daniel Real Sánchez <danielillero@gmail.com>
2025-02-17 22:36:48 +0100

    Crear array inicial, meter imágenes en /public y crear interface

--- BlogNoticias/src/app/app.component.ts
+++ BlogNoticias/src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 
 import { FormularioComponent } from './components/formulario/formulario.component';
 import { ListaNoticiasComponent } from './components/lista-noticias/lista-noticias.component';
+import { INoticia } from './interfaces/inoticia.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [ListaNoticiasComponent],
+  imports: [FormularioComponent, ListaNoticiasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'BlogNoticias';
+  arrayNoticias: INoticia[] = [
+    { titleNoticia: 'Comienza la temporada de gamusinos', dateNoticia: '14-01-2025', imageNoticia: './public/gamusino.jpg', textNoticia: 'There is nothing quite like a relaxing cup of café con leche in Plaza Mayor and a romantic dinner en el Madrid de los Austrias...' },
+    { titleNoticia: 'El alcalde advierte: la aceituna baja', dateNoticia: '31-11-2024', imageNoticia: './public/la aceituna baja.png', textNoticia: 'Es el vecino el que elige al alcalde y es el alcalde el que quiere que sean los vecinos el alcalde. Multiplícate por cero...' }
+  ]
 }
---
+++ BlogNoticias/src/app/interfaces/inoticia.interface.ts
@@ -0,0 +1,6 @@
+export interface INoticia {
+    titleNoticia: string;
+    dateNoticia: string;
+    imageNoticia: string;
+    textNoticia: string;
+}
