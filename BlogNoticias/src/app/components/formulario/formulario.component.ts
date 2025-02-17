10f2907c105e39d89abc9746ba9af55a1caa68b9
Daniel Real Sánchez <danielillero@gmail.com>
2025-02-18 00:36:57 +0100

    Se forma el html del formulario y se inicia el doble biding

--- BlogNoticias/src/app/components/formulario/formulario.component.ts
+++ BlogNoticias/src/app/components/formulario/formulario.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { INoticia } from '../../interfaces/inoticia.interface';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+
+  newNoticia: INoticia = { titleNoticia: '', dateNoticia: '', imageNoticia: '', textNoticia: '' }
 
 }
