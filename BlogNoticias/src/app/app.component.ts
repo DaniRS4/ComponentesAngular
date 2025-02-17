0234698f40636789dda5e64f10c007750f2bfa20
Daniel Real Sánchez <danielillero@gmail.com>
2025-02-17 22:10:35 +0100

    Hemmos creado el componente lista noticias

--- BlogNoticias/src/app/app.component.ts
+++ BlogNoticias/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListaNoticiasComponent } from './components/lista-noticias/lista-noticias.component';
 
 @Component({
   selector: 'app-root',
-  imports: [FormularioComponent],
+  imports: [ListaNoticiasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
